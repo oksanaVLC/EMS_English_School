88104f557e403615fe097228775e0f954abfbdcc
Oksana Golubeva <oksana@uoc.edu>
2026-04-21 23:40:52 +0200

    añadiendo /api en environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ems-backend-production-f455.up.railway.app',
+  apiUrl: 'https://ems-backend-production-f455.up.railway.app/api',
 };
