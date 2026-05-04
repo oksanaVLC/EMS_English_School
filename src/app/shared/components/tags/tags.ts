fd36a9c277af6ee7535ce7072e9c85073b2977f1
Oksana Golubeva <oksana@uoc.edu>
2026-05-04 16:24:37 +0200

    arreglos scroll auto

--- src/app/shared/components/tags/tags.ts
+++ src/app/shared/components/tags/tags.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ScrollService } from '../../../core/services/scroll';
 
 @Component({
   selector: 'app-tags',
@@ -11,10 +10,4 @@ import { ScrollService } from '../../../core/services/scroll';
 })
 export class Tags {
   @Input() items: { label: string; link: string }[] = [];
-
-  constructor(private scroll: ScrollService) {}
-
-  goToSection(id: string) {
-    this.scroll.scrollToElement(id, 80, 'auto');
-  }
 }
